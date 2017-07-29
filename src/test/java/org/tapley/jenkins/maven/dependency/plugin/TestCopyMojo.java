6760eb592e26ca45f33f39431a0b76953dd0d1e1
Chris Tapley <chris.tapley.81@gmail.com>
2017-07-29 21:39:33 +0800

    remove unnecessary includes

--- src/test/java/org/tapley/jenkins/maven/dependency/plugin/TestCopyMojo.java
+++ src/test/java/org/tapley/jenkins/maven/dependency/plugin/TestCopyMojo.java
@@ -17,27 +17,20 @@ package org.tapley.jenkins.maven.dependency.plugin;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.junit.Test;
 import static org.junit.Assert.*;
 import org.junit.Before;
-import org.junit.Rule;
-import org.junit.rules.ExpectedException;
 import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.anyString;
-import org.mockito.Mock;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
-import org.mockito.MockitoAnnotations;
 import org.springframework.test.util.ReflectionTestUtils;
-import org.tapley.jenkins.maven.dependency.plugin.model.JenkinsClient;
 
 /**
  *
